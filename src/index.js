4426ce296f6c4b4b9f5a2956dfff152099d0390b
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-02-08 22:12:59 -0200

    Init IntlProvider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { addLocaleData, IntlProvider } from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import pt from 'react-intl/locale-data/pt'
+import messages from './config/language'
+import { locale, getTranslate as translate } from './functions/utils'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+addLocaleData([...en, ...pt])
+
+ReactDOM.render(
+    <IntlProvider locale={locale} messages={translate(messages[locale])}>
+        <App />
+    </IntlProvider>
+    ,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
